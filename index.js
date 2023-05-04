4e3b66c253c7954b014a7f42bff8f9a47ef02883
Julle Nguyen <jnguyen@appdev.yearup.org>
2023-05-04 09:23:12 -0400

    final

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //basic car rental 29.99/day
 //30% surcharge under 25
-
+"use strict";
+window.onload = function ( ){
+    
+}
 const buttonEl = document.getElementById('estimateBtn');
 buttonEl.onclick = onButtonClicked;
 
@@ -9,14 +12,9 @@ function onButtonClicked(){
     //console.log(inputEl.value);
     let carPerday = 29.99
     let numDays = document.getElementById('numDays2')
-    
     let carRental = numDays.value * carPerday
 
-    let carRentalEl = document.getElementById('carRentalTA')
-    carRentalEl.innerHTML = `$ ${carRental.toFixed(2)}`
-
     let baseP = 0;
-
     if (document.getElementById('tollTag').checked){
         baseP += 3.95;
     }  if (document.getElementById('gps').checked){
@@ -24,27 +22,29 @@ function onButtonClicked(){
     }  if (document.getElementById('roadside').checked){
         baseP += 2.95;
     }
-
     let optionstotal = baseP * numDays.value
 
-    let optionsEl = document.getElementById('optionsTA')
-    optionsEl.innerHTML = `$ ${optionstotal.toFixed(2)}`
-
     let baseP2 = 0
-
     if (document.getElementById('yes').checked){
+
         baseP2 += 30/100
     }
+    
 
     let UnderAgeF = (baseP2 * carRental) 
 
-    let radioEl = document.getElementById('underAge')
-    radioEl.innerHTML = `$ ${UnderAgeF.toFixed(2)}`
-
-
    let grandTotal = UnderAgeF + optionstotal + carRental
 
+   let carRentalEl = document.getElementById('carRentalTA')
+   carRentalEl.innerHTML = `$ ${carRental.toFixed(2)}`
+   
+   let optionsEl = document.getElementById('optionsTA')
+   optionsEl.innerHTML = `$ ${optionstotal.toFixed(2)}`
+   
+   let radioEl = document.getElementById('underAge')
+   radioEl.innerHTML = `$ ${UnderAgeF.toFixed(2)}`
+   
    let totalAmount = document.getElementById('totalDue')
    totalAmount.innerHTML= `$ ${grandTotal.toFixed(2)}`
-
 }
+
